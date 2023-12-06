7077e376128d2ea93ae78d9a7921a2457d09a2d9
waxmask <waxmask45@gmail.com>
2023-12-06 23:23:14 +0300

    KS-2-2 fix problem

--- src/main/kotlin/lesson_2/lesson2_task2.kt
+++ src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -1,14 +1,14 @@
 package lesson_2
 
 fun main() {
-    val employeeNumber : Int = 50
-    val employeeSalary : Int = 30_000
+    val employeeNumber: Int = 50
+    val employeeSalary: Int = 30_000
 
-    val internsNumber : Int = 30
-    val internsSalary : Int = 20_000
+    val internsNumber: Int = 30
+    val internsSalary: Int = 20_000
 
-    val costEmployeeSalary : Int = employeeNumber * employeeSalary // получаем затраты на выплаты постоянным сотрудникам
-    val costInternsSalary : Int =  internsNumber * internsSalary //  получаем затраты на выплаты стажерам сотрудникам
+    val costEmployeeSalary: Int = employeeNumber * employeeSalary // получаем затраты на выплаты постоянным сотрудникам
+    val costInternsSalary: Int = internsNumber * internsSalary //  получаем затраты на выплаты стажерам сотрудникам
 
     val totalExpenses = costEmployeeSalary + costInternsSalary // общие затраты на зп сотрудников
 
