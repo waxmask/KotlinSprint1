236d76bf19866f23a612e0c3e8639a75c56cef94
waxmask <waxmask45@gmail.com>
2023-12-06 23:18:28 +0300

    KS-1-5 fix problem

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,17 +1,19 @@
 package lesson_1
 
 fun main() {
-    val seconds:Int = 6480  // количество секунд, которые Гагарин провел в космосе.
-    val timeString: String = getTimeFromSeconds(seconds)    // вызов функции с сохранением ее возвращаемого значения в переменную
+    val seconds: Int = 6480  // количество секунд, которые Гагарин провел в космосе.
+    val timeString: String =
+        getTimeFromSeconds(seconds)    // вызов функции с сохранением ее возвращаемого значения в переменную
 
     println(timeString) // вывод результата на экран
 }
 
 
 // функция конвертирующая количество секунд в формат времени
-fun getTimeFromSeconds(inputSecondCounter:Int) : String {
-    val hours : Int = inputSecondCounter / 3600             // 3600 секунд в одном часе, просто делим и получаем колво часов
-    val minutes : Int = (inputSecondCounter % 3600) / 60    // берем остаток от деления на 3600 и делим на 60 получаем минуты
-    val second : Int = inputSecondCounter % 60              // просто берем остаток от деления на 60 это оставшиеся секунды
+fun getTimeFromSeconds(inputSecondCounter: Int): String {
+    val hours: Int = inputSecondCounter / 3600  // 3600 секунд в одном часе, просто делим и получаем колво часов
+    val minutes: Int =
+        (inputSecondCounter % 3600) / 60    // берем остаток от деления на 3600 и делим на 60 получаем минуты
+    val second: Int = inputSecondCounter % 60   // просто берем остаток от деления на 60 это оставшиеся секунды
     return String.format("%02d:%02d:%02d", hours, minutes, second)  // форматирум строку и возвращаем ее
 }
