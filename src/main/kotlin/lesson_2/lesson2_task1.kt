2d6b8a18aba7468cea78ad93fb351612e6964654
waxmask <waxmask45@gmail.com>
2023-12-06 23:20:06 +0300

    KS-2-1 fix problem

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -6,17 +6,17 @@ package lesson_2
 – В консоли должно быть выведено дробное число с 2 знаками после запятой.
 */
 fun main() {
-    val studentRatingList : List<Int> = listOf(3,4,3,5) // храним список оценок учеников
+    val studentRatingList: List<Int> = listOf(3, 4, 3, 5) // храним список оценок учеников
 
     println(getAverageScore(studentRatingList)) // вызываем функкцию получения средней оценки и потом передаем ее в функцию println
 }
 
-fun getAverageScore(ratingList: List<Int>) : Float {
-    var averageScore : Float = 0f
+fun getAverageScore(ratingList: List<Int>): Float {
+    var averageScore: Float = 0f
 
-    for (score in ratingList){ // в цикле проходим по каждому элементу списка и накапливаем счетчик
-        averageScore+=score
+    for (score in ratingList) { // в цикле проходим по каждому элементу списка и накапливаем счетчик
+        averageScore += score
     }
 
-    return  averageScore / ratingList.size // а тут получаем среднюю оценку поделив сумму всех оценок на их количество и возвращаем это значение
+    return averageScore / ratingList.size // а тут получаем среднюю оценку поделив сумму всех оценок на их количество и возвращаем это значение
 }
