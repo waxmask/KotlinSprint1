7dcab5001601b386e6e1eb52814e9a2f7ca02e97
waxmask <waxmask45@gmail.com>
2023-12-06 22:57:01 +0300

    KS-1-3 solved problem, fix problem

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -6,14 +6,16 @@ fun main() {
     // объявление переменных. возможно время можно было бы хранить в строке чтобы не дописывать 0 в случае с
     // 0 <= time <= 9 или даже в специальном типе данных, но пока я такого типа не знаю в котлине
     val yearFlight: Int = 1961
-    var hourFlight: Int = 9
-    var minuteFlight: Int = 7
+    var hourFlight: String = "09"
+    var minuteFlight: String = "07"
 
-    println("Год полета - ${yearFlight}" + "\r\n" +     // вывод в консоль сообщения, опять же можно использовать два принта
-            "Взлет - 0${hourFlight}:0${minuteFlight}")
+    println(
+        "Год полета - ${yearFlight}" + "\r\n" +     // вывод в консоль сообщения, опять же можно использовать два принта
+                "\r\nчас: ${hourFlight} \r\nминута: ${minuteFlight}"
+    )
 
-    hourFlight = 10     // присваивание нового "часа"
-    minuteFlight = 55   // присваивание нового "часа"
+    hourFlight = "10"     // присваивание нового "часа"
+    minuteFlight = "55"   // присваивание нового "часа"
 
     print("Посадка - ${hourFlight}:${minuteFlight}")    // вывод сообщения с временем посадки
 }
