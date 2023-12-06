36c6256c535f75018e470c12abc960f2b90a6ba4
waxmask <waxmask45@gmail.com>
2023-12-06 23:11:34 +0300

    KS-1-4 fix problem

--- src/main/kotlin/lesson_1/lesson1_task4.kt
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -1,17 +1,21 @@
 package lesson_1
 
 fun main() {
-    val length:Long = 40_868_600_000
-    val age:Byte = 27
-    val partOfDay:Float = 0.075f // выбрал флоат, так как он занимает меньше памяти чем Double но при этом нужно дописать f после числа
-    val seconds:Short = 6480
-    val partOfYear:Double = 2.0547945205479453E-4
-    val apogee:Int = 327000
+    val length: Long = 40_868_600_000
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
 
-    print("Расстояние $length\n" +
-            "Возраст – $age\n" +
-            "Часть дня – $partOfDay\n" +
-            "Секунды – $seconds\n" +
-            "Часть года – $partOfYear\n" +
-            "Апогей орбиты – $apogee")
+    val outputData = """
+        Расстояние $length
+        Возраст – $age
+        Часть дня – $partOfDay
+        Секунды – $seconds
+        Часть года – $partOfYear
+        Апогей орбиты – $apogee
+    """.trimIndent()
+
+    print(outputData)
 }
